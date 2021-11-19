486790efbe0ffc57daf87df9af260969a938e664
201910835 <oeooho826@naver.com>
2021-11-19 18:11:17 +0900

    경로 수정

--- novelgirls/BERT/src/train_multi_classification.py
+++ novelgirls/BERT/src/train_multi_classification.py
@@ -8,8 +8,8 @@ from bert import run_classifier
 from novelgirls.BERT.src.model import model_fn_builder
 from novelgirls.BERT.src.create_tokenizer import create_tokenizer_from_hub_module as create_tokenizer
 
-OUTPUT_DIR = './BERT/trained_model/bert_eye_color'
-# OUTPUT_DIR = './BERT/trained_model/bert_hair_color'
+''' 모델 저장할 경로 입력'''
+OUTPUT_DIR = ''
 
 DO_DELETE = False
 
