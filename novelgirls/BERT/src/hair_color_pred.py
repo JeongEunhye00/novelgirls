2443a130ca773fa2cb20bc1deb1f15e680dd9482
201910835 <oeooho826@naver.com>
2021-11-19 17:59:49 +0900

    hair_color_infer

--- novelgirls/BERT/src/hair_color_pred.py
+++ novelgirls/BERT/src/hair_color_pred.py
@@ -4,7 +4,7 @@ from model import model_fn_builder
 from novelgirls.BERT.src.create_tokenizer import create_tokenizer_from_hub_module as create_tokenizer
 
 
-MODEL_DIR = './BERT/bert_hair_color'
+MODEL_DIR = './BERT/trained_model/bert_hair_color'
 MAX_SEQ_LENGTH = 200
 BATCH_SIZE = 16
 LEARNING_RATE = 2e-5
